cabeba04c7ce0d296e910cec1d3ddd20b7096646
Alexander Kryukov <akryukov992@gmail.com>
2018-03-30 01:07:09 +0600

    add basics spring boot app with security

---
+++ src/main/java/ru/ominit/Application.java
@@ -0,0 +1,14 @@
+package ru.ominit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Александр on 29.03.2018.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/ru/ominit/MvcConfing.java
@@ -0,0 +1,18 @@
+package ru.ominit;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Александр on 29.03.2018.
+ */
+@Configuration
+public class MvcConfing implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
